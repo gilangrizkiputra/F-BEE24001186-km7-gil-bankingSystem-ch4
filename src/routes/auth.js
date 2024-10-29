8dfd683cf7413693aff7f620dd71c026b8f75878
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-10-29 20:29:39 +0700

    fix:api-docs

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,12 @@
 import express from "express";
 const router = express.Router();
 import { AuthController } from "../controllers/auth.js";
+import { UserController } from "../controllers/user.js";
 
 const auth = new AuthController();
+const user = new UserController();
+
+router.post("/register", (req, res) => {user.createUser(req, res);});
 
 router.post("/login", (req, res) => {auth.login(req, res);});
 
--- src/routes/user.js
+++ src/routes/user.js
@@ -4,8 +4,6 @@ import {  UserController} from "../controllers/user.js";
 
 const user = new UserController();
 
-router.post("/", (req, res) => {user.createUser(req, res);});
-
 router.get("/", (req, res) => {user.getAllUsers(req, res);});
 
 router.get("/:id", (req, res) => {user.getUserById(req, res);});
